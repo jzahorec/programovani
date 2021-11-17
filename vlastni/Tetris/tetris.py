0ab06373d943cce184988316723eef297322f1f9
Jan Záhorec <jan.zahorec@natur.cuni.cz>
2021-11-17 12:37:03 +0100

    Trosku pokrasleny backend tetrisu

--- vlastni/Tetris/tetris.py
+++ vlastni/Tetris/tetris.py
@@ -10,7 +10,7 @@ Provest matiku na toceni dilkem, nejaka matice asi
 import random
 
 """
-Funkce na dilek
+Funkce na matici
 """
 
 def transpozice(matice): #transponuje matici
@@ -27,15 +27,8 @@ def symetrie_radku(matice): #prohodi prvky matice podle osy stredoveho sloupce
             radek[i], radek[delka_radku - (i + 1)] = radek[delka_radku - (i + 1)], radek[i]
     return matice
 
-
-""" zbytecne, nahrazeno prohozenim operaci symetrie_radku a transpozice
-
-def symetrie_sloupcu(matice): #prohodi prvky matice podle osy stredoveho radku
-    pocet_radku = len(matice)
-    for j in range(pocet_radku):
-        for i in range(len(matice[0]) // 2):
-            matice[i][j], matice[pocet_radku - (i + 1)][j] = matice[pocet_radku - (i + 1)][j], matice[i][j]
-    return matice
+"""
+Funkce na dilek
 """
 
 def otoc(dilek, smer): #vrati kontrolni dilek otoceny podle zadani, smer True doleva
@@ -56,38 +49,6 @@ def posun(dilek, smer):
     novy.j = dilek.k + smer[1]
     return novy
 
-"""
-Konec funkci na dilek
-"""
-
-
-"""
-Kontrolni vypis, ze otaceni funguje, jak ma
-
-print(otoc_dilek([[1,0], [1,0], [1,1]], True))
-print(otoc_dilek([[1,0], [1,0], [1,1]], False))
-
-print(otoc_dilek([[0, 1, 0], [1, 1, 1]], True))
- """
-
-"""
-veci pro dilek
-"""
-
-class Dilek:
-    def __init__(self):
-        self.rozmery = []
-        self.i = 0
-        self.j = 0
-
-    def novy_dilek(self, hraci_pole):
-        typy_dilku = [[[1],[1],[1],[1]], [[1, 0], [1, 0], [1, 1]], [[0, 1, 1], [1, 1, 0]], [[1, 1],[1, 1]], [[0, 1, 0], [1, 1, 1]]] #myslim, ze mam vsechny typy dilku
-        self.rozmery = typy_dilku[random.randint(0, len(typy_dilku) - 1)]
-        if random.randint(0, 1):
-            self.rozmery = transpozice(self.rozmery)
-
-        self.i, self.j = 0, len(hraci_pole[0]) // 2 - len(self.rozmery) // 2
-
 def kontrola(hraci_pole, dilek):
     if dilek.i < 0 or dilek.j < 0 or dilek.i >= len(hraci_pole) - len(dilek.rozmery) or dilek.j >= len(hraci_pole[0]) - len(dilek.rozmery[0]): #kontroluje, jestli dilek v poli
         return False
@@ -99,6 +60,10 @@ def kontrola(hraci_pole, dilek):
             if self.hraci_pole[tmp_i + dilek.i][tmp_j + dilek.j] == 1:
                 lze = False
     return lze
+
+"""
+Funkce na hraci pole
+"""
 
 def vynulovani_radku(hraci_pole):
     delka_radku = len(hraci_pole[0])
@@ -124,7 +89,25 @@ def prohra(hraci_pole):
             return True
     return False
 
-class Game:
+"""
+Tridy pro hru
+"""
+
+class Dilek: #instance dilku
+    def __init__(self):
+        self.rozmery = []
+        self.i = 0
+        self.j = 0
+
+    def novy_dilek(self, hraci_pole):
+        typy_dilku = [[[1],[1],[1],[1]], [[1, 0], [1, 0], [1, 1]], [[0, 1, 1], [1, 1, 0]], [[1, 1],[1, 1]], [[0, 1, 0], [1, 1, 1]]] #myslim, ze mam vsechny typy dilku
+        self.rozmery = typy_dilku[random.randint(0, len(typy_dilku) - 1)]
+        if random.randint(0, 1):
+            self.rozmery = transpozice(self.rozmery)
+
+        self.i, self.j = 0, len(hraci_pole[0]) // 2 - len(self.rozmery) // 2
+
+class Game: #instance jedne hry
     def __init__(self, rozmery):
         self.hraci_pole = [[0 for j in range(rozmery[1])] for i in range(rozmery[0])]
         self.score = 0
@@ -156,63 +139,3 @@ class Game:
                 return False, self.body
             else:
                 return True
-
-
-
-
-
-"""
-class Dilek:
-    def __init__(self, hraci_pole, rozmery=[[1, 1], [1, 1]]):
-        self.rozmery = rozmery
-        self.hraci_pole = hraci_pole
-        self.x, self.y = len(self.hraci_pole[0])//2 - len(self.rozmery[0])//2, 0
-
-    def kontrola(self, smer=[1, 0]):
-        lze = True
-        for tmp_x in range(len(self.rozmery)):
-            for tmp_y in range(len(self.rozmery[tmp_x])):
-                if self.hraci_pole[tmp_x + self.x + smer[0]][tmp_y + self.y + smer[1]] == 1:
-                    lze = False
-        return lze
-
-    def levo(self):
-        if self.y > 0:
-            self.y -= 1
-    def pravo(self):
-        if self.y < len(self.hraci_pole[0]) - len(self.rozmery[0]) - 1:
-            self.y += 1
-    def tick(self):
-
-        pass
-
-
-
-vypsani prazdneho pole printem
-
-for radek in hraci_pole:
-    for bod in radek:
-        print(bod, end=" ")
-    print()
-
-
-
-
-
-dilek = Dilek(hraci_pole)
-
-typy_dilku = [[[1],[1],[1],[1]], [[1, 0], [1, 0], [1, 1]], [[0, 1, 1], [1, 1, 0]], [[1, 1],[1, 1]], [[0, 1, 0], [1, 1, 1]]]
-
-dilek = Dilek(dilek.hraci_pole, typy_dilku[random.randint(0, 4)])
-
-dilek.levo()
-
-dilek.levo()
-
-dilek.levo()
-
-dilek.levo()
-
-dilek.levo()
-print(dilek.x, dilek.y)
-"""
